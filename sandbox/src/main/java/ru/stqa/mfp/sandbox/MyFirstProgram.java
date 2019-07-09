899ccc4b2568b4d94647813590ae5db9dffbd991
Vladimir <sukhorukov.v91@gmail.com>
2019-07-09 22:48:24 +0300

    10 урок

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -2,6 +2,10 @@ package ru.stqa.mfp.sandbox;
 
 public class MyFirstProgram {
   public static void main (String[] args) {
-    System.out.println("Hello, World!");
+    System.out.println("Hello, " + "World!");
+
+    int l = 8;
+    int s =l*l;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
   }
 }
