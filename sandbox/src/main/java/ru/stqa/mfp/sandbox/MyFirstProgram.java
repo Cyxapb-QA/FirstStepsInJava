21a5ba11966bc8130f6bd04620f1dc07fa397198
Cyxapb <sukhorukov.v91@gmail.com>
2019-07-10 14:55:10 +0300

    добавил несколько методов

--- sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/mfp/sandbox/MyFirstProgram.java
@@ -1,11 +1,29 @@
 package ru.stqa.mfp.sandbox;
 
 public class MyFirstProgram {
-  public static void main (String[] args) {
-    System.out.println("Hello, " + "World!");
 
-    int l = 8;
-    int s =l*l;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + s);
+  public static void main (String[] args) {
+    hello("World");
+    hello("User");
+    hello("Alexey");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + "=" + area(l));
+
+    double a = 4;
+    double b = 6;
+    System.out.println("Площадь теругольника со сторонами "+ a + " и " + b + " = " + area(a,b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area (double len) {
+    return len*len;
+  }
+
+  public static double area (double a, double b){
+    return a * b;
   }
 }
